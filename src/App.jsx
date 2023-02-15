200db8488f09b6a0ce0f144623b190b62eec5aef
tomhoma <theera799@gmail.com>
2023-02-16 03:14:15 +0700

    add ing

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Click is {count}
+          Clicking is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
